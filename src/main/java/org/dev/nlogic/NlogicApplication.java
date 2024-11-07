29fbf88fb2671cddb8825f287681d3ef85b96e17
jihwan <jihwan1535@nate.com>
2024-11-07 16:46:26 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/org/dev/nlogic/NlogicApplication.java
@@ -0,0 +1,13 @@
+package org.dev.nlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NlogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dev/nlogic/NlogicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.nlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
