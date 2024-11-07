8f691260fd39733221ccb3df438319438f2ac1b4
jihwan <jihwan1535@nate.com>
2024-11-08 00:09:35 +0900

    test: test 22

---
+++ src/test/java/org/dev/nlogic/TempTest.java
@@ -0,0 +1,16 @@
+package org.dev.nlogic;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TempTest {
+
+    @Test
+    void test() {
+        // Jacoco Test Report
+        assertThat(1).isOne();
+    }
+
+}
